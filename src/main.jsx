bb03a1f9b509912aeb9512ce16bfbbe3fd0115aa
sohan-r30 <sohanakondo139@gmail.com>
2023-10-08 12:51:06 +0600

    the jewelry muse project setup

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <div>
+      <p className='text-5xl'>hi</p>
+    </div>
+  </React.StrictMode>,
+)
